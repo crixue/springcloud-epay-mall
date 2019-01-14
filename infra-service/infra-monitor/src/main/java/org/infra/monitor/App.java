6e2a4fbc05a1a893998c5f9065f9a4ebb71a8522
crixue <crixue@qq.com>
2019-01-15 00:17:20 +0800

    监控项目

--- infra-service/infra-monitor/src/main/java/org/infra/monitor/App.java
+++ infra-service/infra-monitor/src/main/java/org/infra/monitor/App.java
@@ -1,13 +1,15 @@
 package org.infra.monitor;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
